8521ad3c0bfaa93d7fcece87558a7136b5d997d1
Ryan Flores <ryan.designe.ux@outlook.com>
2023-12-27 00:02:53 -0300

    final service for get cards

--- src/services/getCardService.js
+++ src/services/getCardService.js
@@ -7,16 +7,16 @@ class GetCardService {
   async descriptografarDado(iv, criptografado, tag, chave) {
     return new Promise((resolve, reject) => {
       const chaveDeCriptografia = chave;
-      const keyLength = 32; 
+      const keyLength = 32;
       const paddedKey = Buffer.alloc(keyLength, chaveDeCriptografia, 'utf-8');
       const decipher = crypto.createDecipheriv('aes-256-gcm', paddedKey, Buffer.from(iv, 'hex'));
 
       decipher.setAuthTag(Buffer.from(tag, 'hex'));
-      
+
       let descriptografado = decipher.update(criptografado, 'hex', 'utf-8');
 
       descriptografado += decipher.final('utf-8');
-      
+
       resolve(descriptografado);
     });
   }
@@ -28,19 +28,17 @@ class GetCardService {
       const pool = await this.database.createPool(req.user.database);
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM card');
-      const [numero, cvv] = await Promise.all(
-        result.rows.map(row => (
-          this.descriptografarDado(row.card_nm_iv, row.card_nm_cpt, row.card_nm_tag, row.card_key_secret)
-        ),
-        result.rows.map(row => (
-          console.log(row.card_cvv_iv, row.card_cvv_cpt, row.card_cvv_tag, row.card_key_secret)
-          // this.descriptografarDado(row.card_cvv_iv, row.card_cvv_cpt, row.card_cvv_tag, row.card_key_secret)
-        )))
+      const resultados = await Promise.all(
+        result.rows.map(async x => {
+          const numeroCartao = await this.descriptografarDado(x.card_nm_iv, x.card_nm_cpt, x.card_nm_tag, x.card_key_secret);
+          const cvv = await this.descriptografarDado(x.card_cvv_iv, x.card_cvv_cpt, x.card_cvv_tag, x.card_key_secret);
+
+          return { numeroCartao, cvv };
+        })
       );
 
-      return {
-        numero
-      };
+      
+      return resultados;
     } catch (error) {
       throw error;
     }
